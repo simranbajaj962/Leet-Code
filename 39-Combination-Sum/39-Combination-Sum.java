41dc57af3c10a7b59eb64200d07ff9fed18f7ca3
Simran Bajaj <simranbajaj962@gmail.com>
2023-01-04 12:59:41 +0530

    Runtime: 3 ms, Memory: 45.1 MB

--- 39-Combination-Sum/39-Combination-Sum.java
+++ 39-Combination-Sum/39-Combination-Sum.java
@@ -1,3 +1,37 @@
+
+// Runtime: 3ms
+// Memory : 45.1MB
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findcomb(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+
+    }
+
+    private void findcomb(int index, int arr[], int target, List<List<Integer>> ans, List<Integer> ds) {
+        if (index == arr.length) {
+            if (target == 0) {
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        if (arr[index] <= target) {
+            ds.add(arr[index]);
+            findcomb(index, arr, target - arr[index], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+        findcomb(index + 1, arr, target, ans, ds);
+    }
+
+}
+
+
+
+// Runtime : 8ms 
+// Memory : 44.9MB
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList();
