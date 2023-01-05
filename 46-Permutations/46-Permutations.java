159a19ab1ae0585065c8bcef107d1baa5f3d5b88
Simran Bajaj <simranbajaj962@gmail.com>
2023-01-05 14:13:10 +0530

    Runtime: 3 ms, Memory: 45 MB

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,3 +1,6 @@
+// Time Complexity: (n! * n)
+// Space Complexity: O(n) + O(n)
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -21,5 +24,38 @@ class Solution {
                 freq[i] = false;
             }
         }
+    }
+}
+
+// Time Complexity: (n! * n)
+// Space Complexity: O(n) + O(n!)
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findPermutations(0,nums,ans);
+        return ans;
+    }
+
+    private void findPermutations(int index, int nums[],List<List<Integer>> ans){
+        if(index == nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=index; i<nums.length;i++){
+            swap(i,index,nums);
+            findPermutations(index+1,nums,ans);
+            swap(i,index,nums);
+        }
+    }
+
+    private void swap(int i,int j,int nums[]){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
