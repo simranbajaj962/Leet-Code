c31e8d1a3c01373cedab066431a2d28a74b02a5f
Simran Bajaj <simranbajaj962@gmail.com>
2023-01-08 19:02:17 +0530

    Runtime: 1 ms, Memory: 43.7 MB

--- 977-Squares-of-a-Sorted-Array/977-Squares-of-a-Sorted-Array.java
+++ 977-Squares-of-a-Sorted-Array/977-Squares-of-a-Sorted-Array.java
@@ -10,3 +10,46 @@ class Solution {
         return nums;
     }
 }
+
+// Approach 2 (Two pointer approach)
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int a=0;
+        int i=0;
+        int j=0;
+
+        for(int k=0;k<nums.length;k++){
+            if(nums[k] < 0) j++;
+            nums[k]=nums[k]*nums[k];
+        }
+        i=j-1;
+
+        while(i>=0 && j<nums.length){
+            if(nums[i] < nums[j]){
+                result[a]=nums[i];
+                i--;
+            }else{
+                result[a]=nums[j];
+                j++;
+            }
+            a++;
+        }
+
+        while(i>=0){
+            result[a]=nums[i];
+            i--;
+            a++;
+        }
+
+        while(j<nums.length){
+            result[a] = nums[j];
+            j++;
+            a++;
+        }
+        return result;
+    }
+}
