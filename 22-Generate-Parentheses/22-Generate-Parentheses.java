01842aa8f3c55e8f4b3e11792f13f6e173807679
Simran Bajaj <72919255+simranbajaj962@users.noreply.github.com>
2023-01-14 14:54:44 +0530

    added different code

--- 22-Generate-Parentheses/22-Generate-Parentheses.java
+++ 22-Generate-Parentheses/22-Generate-Parentheses.java
@@ -1,3 +1,5 @@
+// This file contains two different codes (Approach is same)
+
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
@@ -18,5 +20,31 @@ class Solution {
         if (closeCount < openCount) {
             backtrack(openCount, closeCount + 1, n, res, s + ")");
         }
+    }
+}
+
+// another code for the same:-
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        
+        List<String> list = new ArrayList<String>();
+        vp(n,0,0,"",list);
+        return list;
+        
+    }
+    
+    public static void vp(int n , int open , int close, String ans, List<String> list){
+        if (open == n && close == n){
+           list.add(ans);
+            return;
+        }
+        if (open > n || close > open){
+            return;
+        }
+        
+        vp(n, open+1, close, ans + "(" , list);
+        vp(n, open, close+1, ans + ")", list);
+
+        
     }
 }
