1ad26d85ff614c1dfc1fa2ae7828d9a2a585ac94
Simran Bajaj <simranbajaj962@gmail.com>
2023-01-18 13:47:08 +0530

    Runtime: 12ms, Memory: 50MB

---
+++ 148-Sort-List/148-Sort-List.java
@@ -0,0 +1,39 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = middleNode(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        current.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        return mid;
+    }
+}
---
+++ 148-Sort-List/README.md
@@ -0,0 +1,30 @@
+<h2>148. Sort List</h2>
+<h3>Medium</h3>
+<hr>
+<div>
+<p>Given the head of a linked list, return the list after sorting it in ascending order.</p>
+
+<p><b>Example 1: </b></p>
+<pre>
+<img src="https://assets.leetcode.com/uploads/2020/09/14/sort_list_1.jpg">
+<strong>Input:</strong> head = [4,2,1,3]
+<strong>Output:</strong> [1,2,3,4]
+</pre>
+
+<p><b>Example 2: </b></p>
+<pre>
+<img src="https://assets.leetcode.com/uploads/2020/09/14/sort_list_2.jpg"><strong>Input:</strong> head = [-1,5,3,4,0]
+<strong>Output:</strong> [-1,0,3,4,5]
+</pre>
+
+<p><b>Example 3: </b></p>
+<pre><strong>Input:</strong> head = []
+<strong>Output:</strong> []
+</pre>
+
+<p><b>Constraints:</b></p>
+<ul> 
+    <li>The number of nodes in the list is in the range [0, 5 * 10⁴]</li>
+    <li>-10⁵ <= Node.val <= 10⁵</li>
+</ul>
+</div>
