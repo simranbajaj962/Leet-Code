fcf9b9b9d9931d6cea797070a322a357dc31a46e
Simran Bajaj <simranbajaj962@gmail.com>
2023-05-03 19:54:36 +0530

    Runtime: 1ms, Memory: 43.3MB

--- 203-Remove-Linked-List-Elements/203-Remove-Linked-List-Elements.java
+++ 203-Remove-Linked-List-Elements/203-Remove-Linked-List-Elements.java
@@ -1,4 +1,24 @@
-// Using Stack -
+//-----------Iterative approach--------------------------
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
+
+// -------------------Using Stack----------------------------------
 
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
