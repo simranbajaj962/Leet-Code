c633fb9766b046e19a62b0eaf0504296c1991f61
Simran Bajaj <simranbajaj962@gmail.com>
2023-05-03 19:29:26 +0530

    Runtime: 4 ms, Memory:43.7 MB

---
+++ 203-Remove-Linked-List-Elements/203-Remove-Linked-List-Elements.java
@@ -0,0 +1,30 @@
+// Using Stack -
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode curr = head;
+
+        // simply push all the values other than those
+        // that needs to be deleted from the list
+        while (curr != null) {
+            if (curr.val != val) {
+                stack.push(curr);
+            }
+            curr = curr.next;
+        }
+        // now there are only those values in the stack
+        // which should be present after deletion
+
+        ListNode newHead = null;
+
+        // taking out all the values from
+        // stack one by one and returning the new head.
+        while (!stack.isEmpty()) {
+            ListNode node = stack.pop();
+            node.next = newHead;
+            newHead = node;
+        }
+        return newHead;
+    }
+}
---
+++ 203-Remove-Linked-List-Elements/README.md
@@ -0,0 +1,33 @@
+<h2>203. Remove Linked List Elements</h2>
+<h3>Easy</h3>
+<hr>
+<div>
+<p>Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+</p>
+
+<p><b>Example 1: </b></p>
+<pre>
+<img src="https://assets.leetcode.com/uploads/2021/03/06/removelinked-list.jpg">
+<strong>Input:</strong> head = [1,2,6,3,4,5,6], val = 6
+<strong>Output:</strong> [1,2,3,4,5]
+</pre>
+
+<p><b>Example 2: </b></p>
+<pre>
+<strong>Input:</strong> head = [], val = 1
+<strong>Output:</strong> []
+</pre>
+
+<p><b>Example 3: </b></p>
+<pre>
+<strong>Input:</strong> head = [7,7,7,7], val = 7
+<strong>Output:</strong> []
+</pre>
+
+<p><b>Constraints:</b></p>
+<ul> 
+    <li>The number of nodes in the list is in the range [0, 10⁴]</li>
+    <li>1 <= Node.val <= 50</li>
+    <li>0 <= val <= 50</li>
+</ul>
+</div>
