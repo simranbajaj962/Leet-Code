3732d4af487c896ea8f0a4e9e8c5891180547d8d
Simran Bajaj <simranbajaj962@gmail.com>
2023-05-04 17:08:59 +0530

    Runtime: 2ms, Memory: 42.8MB

--- 1-Two-Sum/1-Two-Sum.java
+++ 1-Two-Sum/1-Two-Sum.java
@@ -11,3 +11,20 @@ class Solution {
         return new int[] { -1, -1 };
     }
 }
+
+// -----------------Using map------------------------
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] { -1, -1 };
+    }
+}
