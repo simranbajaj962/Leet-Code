38a981a673cfab520506719eefcabb8e762a413c
Simran Bajaj <simranbajaj962@gmail.com>
2023-05-04 17:50:12 +0530

    Runtime: 2ms, Memory: 59.4MB

--- 121-Best-Time-to-Buy-and-Sell-Stock/121-Best-Time-to-Buy-and-Sell-Stock.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock/121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = prices[0];
+        int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for(int i=1; i<prices.length;i++){
-            if(prices[i]<buy){
-                buy = prices[i];
-            }else if(prices[i] - buy > maxProfit){
-                maxProfit = prices[i] -buy;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
             }
         }
         return maxProfit;
