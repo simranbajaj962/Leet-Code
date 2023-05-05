7c3739d4e80bb98d657858d4127aa1c2d1594287
Simran Bajaj <simranbajaj962@gmail.com>
2023-05-05 11:33:20 +0530

    Runtime: 1ms, Memory: 44.3MB

--- 977-Squares-of-a-Sorted-Array/977-Squares-of-a-Sorted-Array.java
+++ 977-Squares-of-a-Sorted-Array/977-Squares-of-a-Sorted-Array.java
@@ -3,8 +3,8 @@
 // Space Complexity: O(1)
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
         }
         Arrays.sort(nums);
         return nums;
@@ -16,40 +16,20 @@ class Solution {
 // Space Complexity: O(N)
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] result = new int[nums.length];
+        int res[] = new int[nums.length];
 
-        int a=0;
-        int i=0;
-        int j=0;
+        int start = 0, end = nums.length - 1;
+        int resIdx = nums.length - 1;
 
-        for(int k=0;k<nums.length;k++){
-            if(nums[k] < 0) j++;
-            nums[k]=nums[k]*nums[k];
-        }
-        i=j-1;
-
-        while(i>=0 && j<nums.length){
-            if(nums[i] < nums[j]){
-                result[a]=nums[i];
-                i--;
-            }else{
-                result[a]=nums[j];
-                j++;
+        while (start <= end) {
+            if (nums[start] * nums[start] > nums[end] * nums[end]) {
+                res[resIdx--] = nums[start] * nums[start];
+                start++;
+            } else {
+                res[resIdx--] = nums[end] * nums[end];
+                end--;
             }
-            a++;
         }
-
-        while(i>=0){
-            result[a]=nums[i];
-            i--;
-            a++;
-        }
-
-        while(j<nums.length){
-            result[a] = nums[j];
-            j++;
-            a++;
-        }
-        return result;
+        return res;
     }
 }
