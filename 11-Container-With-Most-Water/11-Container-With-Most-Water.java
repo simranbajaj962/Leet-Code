4a725d2b69a6298dfe09efb85d4f08a5e85685eb
Simran Bajaj <simranbajaj962@gmail.com>
2023-05-06 09:44:43 +0530

    Runtime: 4ms, Memory: 52.9MB

--- 11-Container-With-Most-Water/11-Container-With-Most-Water.java
+++ 11-Container-With-Most-Water/11-Container-With-Most-Water.java
@@ -1,23 +1,39 @@
+//---------Brute force--------------
 class Solution {
     public int maxArea(int[] height) {
-        int start = 0;
-        int end = height.length - 1;
-        int water = 0;
-
-        while (start <= end) {
-            // width of the contained water
-            int w = end - start;
-            // height of the contained water
-            int h = Math.min(height[start], height[end]);
-
-            // area of the conatiner with water
-            water = Math.max(water, w * h);
-
-            if (height[start] < height[end])
-                start++;
-            else
-                end--;
+        int max = 0;
+        for (int i = 0; i < height.length - 1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int water = Math.min(height[i], height[j]) * (j - i);
+                max = Math.max(max, water);
+            }
         }
-        return water;
+        return max;
+    }
+}
+
+// ---------Two-pointer approach---------------
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+
+        int maxWater = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = width * h;
+
+            maxWater = Math.max(area, maxWater);
+
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
     }
 }
